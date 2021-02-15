e4b7e447426da81b800039d05d1a6e6cc9cc957b
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-02-15 14:20:45 -0800

    added views/urls_show.ejs and configured express server to get and render response

--- express_server.js
+++ express_server.js
@@ -9,21 +9,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+ 
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.slice(0, -1)]};
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
